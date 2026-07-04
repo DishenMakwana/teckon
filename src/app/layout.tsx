0f3c778b8c608f8bb91cd4066a52770c3cad0742
dishen <dishenmakwana.dm@gmail.com>
2026-07-04 19:57:30 +0530

    feat: add Google Tag Manager for enhanced tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -188,8 +188,30 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google Tag Manager */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W5456QX9');
+            `,
+          }}
+        />
       </head>
       <body className="font-sans bg-white text-gray-900 antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W5456QX9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Preloader />
 
         {/* <TopInfoBar /> */}
